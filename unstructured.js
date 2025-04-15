6f9e4a37c75a87d5d26f434d794ba33fc5d69e3a
Scott Taylor <sgtaylor16@gmail.com>
2025-04-15 13:00:19 -0400

    Added unstructured

---
+++ unstructured.js
@@ -0,0 +1,34 @@
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+import {tricontour} from "https://cdn.skypack.dev/d3-tricontour@1";
+
+
+function linspace(start,stop,num){
+    const step = (stop-start)/(num-1);
+    return Array.from({length:num},(d,i) => start + i*step);
+}
+
+
+function unstructured(data,valuerange,svgtag,levels=8){
+    //
+
+    const svg = d3.select(svgtag);
+    const svg_width = +svg.attr('width');
+    const svg_height = +svg.attr('height');
+    const margin = {top:10,right:10,bottom:10,left:10};
+
+    const xscale = d3.scaleLinear().domain(d3.extent(data,d => d[0])).range([margin.left,svg_width - margin.right]);
+    const yscale = d3.scaleLinear().domain(d3.extent(data,d => d[1])).range([svg_height - margin.bottom,margin.top]);
+
+    const levels = linspace(valuerange[0],valuerange[1],levels);
+
+    contours = tricontour().thresholds(levels)(data);
+
+    path = d3.geoPath()
+
+    svg.selectAll("path")
+    .data(contours)
+    .join("path")
+    .attr("d",path)
+    .attr("stroke","black")
+
+}
